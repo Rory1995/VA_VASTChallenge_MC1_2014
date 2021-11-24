75665f4f48132203aac7271ec9e070c1d87ab929
roberta <r.pappolla@studenti.unipi.it>
2021-11-24 20:09:35 +0100

    add navbar
    aggiunta barra per la navigazione personalizzata

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top" >
+      <a class="navbar-brand"  href="#"><img src="./assets/logo.png" class="d-inline-block align-top" alt="logo">
+        VAST Challenge 2014
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Mini Challenge 1 <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const d3 = require('d3');
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+
+  mounted(){
+    let numbers = [190,230, 160, 80, 230];
+    const svg = d3.select('#viz');
+
+    svg.selectAll('rect')
+        .data(numbers)
+        .join('rect')
   }
 }
 </script>
@@ -21,7 +44,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
